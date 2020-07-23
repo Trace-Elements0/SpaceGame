5259cc5ea644d3a5cf868f08611d70988441fced
Jorge <62453774+Trace-Elements0@users.noreply.github.com>
2020-07-23 10:37:54 -0400

    Add files via upload

---
+++ SpaceGameTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using System;
+using static ClassLibrary.Enumerations;
+
+namespace SpaceGameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheMerchant player = new TheMerchant();
+            Weapons musket = new Weapons();
+            musket.WeaponType = WeaponTypes.Musket;
+            player.Weapons.Add(musket);
+        }
+    }
+}
