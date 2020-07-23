5b909cba62e8092ca41995fd605b6f7be8c2e2be
Trace-Elements0 <62453774+Trace-Elements0@users.noreply.github.com>
2020-07-23 14:54:39 -0400

    48546546
    6545151564

---
+++ SpaceGameTest/Alien.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Alien : Characters
+    {
+        public Alien()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int AlienCurrency
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void KillMerchant()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
